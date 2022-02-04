9f23b1c514143e51997b53fa1aefa0a9dd953698
Cleanteuh <98952240+Cleanteuh@users.noreply.github.com>
2022-02-04 17:02:10 +0100

    Add files via upload

---
+++ dminer.py
@@ -0,0 +1,16 @@
+from random import randint
+liste_position = []
+mine = 0
+while mine < 20:
+    x=randint(1, 10)
+    y=randint(1, 10)
+    if [x,y] in liste_position:
+        break
+    else:
+        liste_position.append([x,y])
+        mine+=1
+
+print(liste_position)
+
+
+
