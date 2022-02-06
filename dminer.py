0b33a661f28203b34b70afff80f79d506b5cb876
Cleanteuh <98952240+Cleanteuh@users.noreply.github.com>
2022-02-06 11:37:44 +0100

    Update dminer.py

--- dminer.py
+++ dminer.py
@@ -4,13 +4,9 @@ mine = 0
 while mine < 20:
     x=randint(1, 10)
     y=randint(1, 10)
-    if [x,y] in liste_position:
-        break
-    else:
+    while [x,y] not in liste_position:
         liste_position.append([x,y])
         mine+=1
 
 print(liste_position)
-
-
 
