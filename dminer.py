d961c39f6548ccb9cbe0044d75bcd5770a117f31
Cleanteuh <98952240+Cleanteuh@users.noreply.github.com>
2022-02-06 12:48:34 +0100

    Update dminer.py

--- dminer.py
+++ dminer.py
@@ -1,12 +1,38 @@
-from random import randint
+"""
+    0: vide
+    1: bombe
+    2: drapeau
+
+"""
+
+def generate_grid():
+    grid = {}
+    for colonne in range(1, 11):
+        for ligne in range(1, 11):
+            grid[(colonne, ligne)] = ["0", "0"]  # [etat, nombre_de_mines_autour]
+    return grid
+
+grid = generate_grid()
+
+
+
+"""création de 20 mines aléatoires"""
+
 liste_position = []
 mine = 0
 while mine < 20:
-    x=randint(1, 10)
-    y=randint(1, 10)
-    while [x,y] not in liste_position:
-        liste_position.append([x,y])
+    x=randint(1, 11)
+    y=randint(1, 11)
+    if [x,y] not in liste_position:
+        liste_position.append((x,y))
         mine+=1
-
+"""
 print(liste_position)
+"""
+for i in range(20):
+    [x,y] = liste_position[0]
+    if liste_position[0] in grid:
+        grid[(x,y)] = [1, 0]
+        liste_position.remove(liste_position[0])
 
+print(grid)
