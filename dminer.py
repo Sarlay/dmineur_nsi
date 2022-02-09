36ef4b3549b9187924506a3e625be17256804a7a
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-02-09 21:54:24 +0000

    Update dminer.py

--- dminer.py
+++ dminer.py
@@ -45,9 +45,11 @@ def calculate_near_sum(coo, grid):
     for x in range(-1, 2):
         for y in range(-1, 2):
             if x != 0 or y != 0:
-                if grid[(int(coo[0]-x), coo[1]-y)][0] == "-1":
-                   print(f"true, in {x}, {y}")
-                   near_sum += 1
+                try:
+                    if grid[(int(coo[0]-x), coo[1]-y)][0] == "-1":
+                        near_sum += 1
+                except:
+                    pass
     return near_sum
             
 
@@ -70,42 +72,70 @@ def show_debug_grid(grid):
             print(type_element, end=" ")
         print("|") # closes the right side of the grid
     print("    --------------------------------")
-'''
+
+
+
+def show_player_grid(grid):
+    print("""
+====    grille démineur:  ====
+""")
+    print("      1  2  3  4  5  6  7  8  9  10")
+    print("    ________________________________")
+    for x in range(1, 11):
+        if x == 10: ## for the last line
+            print(f"{x} | ", end="")  # remove a space because of the 2 digits causing line shift 
+        else:
+            print(f"{x}  | ", end="")
+        for y in range(1, 11):
+            if grid[(x, y)][2] is True: # if element is discovered by player: print it
+                print(" " + str(calculate_near_sum((x, y), grid)), end=" ")
+            else:
+                print(" .", end=" ")
+        print("|") # closes the right side of the grid
+    print("    --------------------------------")
+
+"""
 def generate_grid_player():
     grid_player = {}
     for colonnex in range(1, 11):
         for ligney in range(1, 11):
             grid[(colonnex, ligney)] = ["."]  
     return grid_player
-'''
+"""
+
 list_cases = []
 for nb in range(101):
     list_cases.append(".")
     
 
-choix_joueur = int(input("Que voulez vous faire ?, découvrir une case: 1 ; planter un drapeau: 2"))
-xpoint = []
-ypoint = []
-if choix_joueur = 1:
-    if 1 < positionx_joueur and positiony_joueur < 10:
-        positionx_joueur = int(input("saisissez l'abscisse de la case"))
-        positiony_joueur = int(input("saisissez l'ordonnée de la case"))
-        grid[(xpoint)] = [positionx_joueur]
-        grid[(ypoint)] = [positiony_joueur]
-else:
-    positionx_drapeau = int(input("saisissez l'abscisse de la case"))
-    positiony_drapeau = int(input("saisissez l'ordonnée de la case"))
-    if 1 < positionx_drapeau and positiony_drapeau < 10:
-        
+       
       
-print("""
-====    grille secrète:  ====
-""")
-print("      1  2  3  4  5  6  7  8  9  10")
-print("    ________________________________")
-
-
-grid_player = generate_grid_player()
 grid = generate_grid()
 liste_position = generate_mines()
 add_mines_to_grid(liste_position, grid)
+
+while True:
+    show_player_grid(grid)
+    choix_joueur = str(input("Que voulez vous faire ?, découvrir une case: c ; planter un drapeau: d\n "))
+    if choix_joueur == "c":
+        positionx_joueur = int(input("saisissez l'abscisse de la case: "))
+        positiony_joueur = int(input("saisissez l'ordonnée de la case: "))
+        if 1 <= positionx_joueur <= 10 and 1 <= positiony_joueur <= 10: # check if entry exists     
+            if grid[(positionx_joueur, positiony_joueur)][2] is False:  # if case hasn't been discovered yet:
+                if grid[(positionx_joueur, positiony_joueur)][0] == "-1":  # :
+                    print("BOOM !")                    
+                    grid[(positionx_joueur, positiony_joueur)][2] = True  # :
+                else:
+                    print("pas de mine à cet endroit")
+                    grid[(positionx_joueur, positiony_joueur)][2] = True  # :
+            else:
+                print("case déjà découverte !")
+        else:
+            print("case invalide")
+    elif choix_joueur == "d":
+        positionx_drapeau = int(input("saisissez l'abscisse de la case"))
+        positiony_drapeau = int(input("saisissez l'ordonnée de la case"))
+        if 1 < positionx_drapeau and positiony_drapeau < 10:
+            print("idk")
+    elif choix_joueur == "nsi":
+        show_debug_grid(grid)
