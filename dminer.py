1bffcd230e60d4da5ea600e0206312d330f9a4f7
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-02-10 11:03:12 +0100

    Update dminer.py

--- dminer.py
+++ dminer.py
@@ -14,8 +14,9 @@ def generate_grid():
     return grid
 
 
-"""création de 20 mines aléatoires"""
+
 def generate_mines():
+    """create the position of 20 random mines"""
     liste_position = []
     mine = 0
     while mine < 20:
@@ -26,9 +27,7 @@ def generate_mines():
             mine+=1
     return liste_position
 
-"""
-print(liste_position)
-"""
+
 def add_mines_to_grid(liste_position, grid):
     for i in range(20):
         if liste_position[0] in grid:
@@ -37,8 +36,6 @@ def add_mines_to_grid(liste_position, grid):
             liste_position.remove(liste_position[0])
     return grid
 
-            
-            
 
 def calculate_near_sum(coo, grid):
     near_sum = 0
@@ -51,7 +48,6 @@ def calculate_near_sum(coo, grid):
                 except:
                     pass
     return near_sum
-            
 
     
 def show_debug_grid(grid):
@@ -60,12 +56,12 @@ def show_debug_grid(grid):
 """)
     print("      1  2  3  4  5  6  7  8  9  10")
     print("    ________________________________")
-    for x in range(1, 11):
-        if x == 10: ## for the last line
-            print(f"{x} | ", end="")  # remove a space because of the 2 digits causing line shift 
+    for y in range(1, 11):
+        if y == 10: ## for the last line
+            print(f"{y} | ", end="")  # remove a space because of the 2 digits causing line shift 
         else:
-            print(f"{x}  | ", end="") 
-        for y in range(1, 11):
+            print(f"{y}  | ", end="") 
+        for x in range(1, 11):
             type_element = grid[(x, y)][0]
             if type_element.startswith("-") is False:
                 type_element = " " + type_element  # fixes '-' line shift
@@ -81,12 +77,12 @@ def show_player_grid(grid):
 """)
     print("      1  2  3  4  5  6  7  8  9  10")
     print("    ________________________________")
-    for x in range(1, 11):
-        if x == 10: ## for the last line
-            print(f"{x} | ", end="")  # remove a space because of the 2 digits causing line shift 
+    for y in range(1, 11):
+        if y == 10: ## for the last line
+            print(f"{y} | ", end="")  # remove a space because of the 2 digits causing line shift 
         else:
-            print(f"{x}  | ", end="")
-        for y in range(1, 11):
+            print(f"{y}  | ", end="")
+        for x in range(1, 11):
             if grid[(x, y)][2] is True: # if element is discovered by player: print it
                 print(" " + str(calculate_near_sum((x, y), grid)), end=" ")
             else:
@@ -94,22 +90,8 @@ def show_player_grid(grid):
         print("|") # closes the right side of the grid
     print("    --------------------------------")
 
-"""
-def generate_grid_player():
-    grid_player = {}
-    for colonnex in range(1, 11):
-        for ligney in range(1, 11):
-            grid[(colonnex, ligney)] = ["."]  
-    return grid_player
-"""
 
-list_cases = []
-for nb in range(101):
-    list_cases.append(".")
-    
-
-       
-      
+# the main function
 grid = generate_grid()
 liste_position = generate_mines()
 add_mines_to_grid(liste_position, grid)
@@ -117,25 +99,26 @@ add_mines_to_grid(liste_position, grid)
 while True:
     show_player_grid(grid)
     choix_joueur = str(input("Que voulez vous faire ?, découvrir une case: c ; planter un drapeau: d\n "))
-    if choix_joueur == "c":
+    if choix_joueur == "c" or choix_joueur == "d":
         positionx_joueur = int(input("saisissez l'abscisse de la case: "))
         positiony_joueur = int(input("saisissez l'ordonnée de la case: "))
         if 1 <= positionx_joueur <= 10 and 1 <= positiony_joueur <= 10: # check if entry exists     
             if grid[(positionx_joueur, positiony_joueur)][2] is False:  # if case hasn't been discovered yet:
                 if grid[(positionx_joueur, positiony_joueur)][0] == "-1":  # :
+                    if choix_joueur == "c":
+                        print("BOOM !")
+                    else:
+                        grid[(positionx_joueur, positiony_joueur)][0] = "-2" # replace type of element with a flag at coordinates 
                     print("BOOM !")                    
-                    grid[(positionx_joueur, positiony_joueur)][2] = True  # :
+                    grid[(positionx_joueur, positiony_joueur)][2] = True  # mark the case as discovered
                 else:
                     print("pas de mine à cet endroit")
+                    if choix_joueur == "d":
+                        print("Pas de mine à cet endroit !")
                     grid[(positionx_joueur, positiony_joueur)][2] = True  # :
             else:
                 print("case déjà découverte !")
         else:
             print("case invalide")
-    elif choix_joueur == "d":
-        positionx_drapeau = int(input("saisissez l'abscisse de la case"))
-        positiony_drapeau = int(input("saisissez l'ordonnée de la case"))
-        if 1 < positionx_drapeau and positiony_drapeau < 10:
-            print("idk")
     elif choix_joueur == "nsi":
         show_debug_grid(grid)
